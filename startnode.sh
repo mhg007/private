dee7733526bc58b36af710951502abb665b255af
mhg007 <mhasnain753@gmail.com>
2022-12-02 21:16:55 +0500

    set up first ethereum private node on linux

---
+++ startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --miner.threads 2 --datadir "." --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --nat "any" --http.api eth,web3,personal,net,miner --unlock 0 --allow-insecure-unlock --password ./password.sec
